91546afe253071bb9233c9eed5c73992d4224ffa
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-05 14:23:18 -0700

    add examples of conditional in for loop + break and continue usages

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,25 @@ public class Main {
         for (int i = 0;  i <= 10; i++) {
             System.out.println("i = " + i);
         }
-
+        // basic for loop decrement
         for (int j = 10; j >= 0; j--) {
             System.out.println("j = " + j);
+        }
+        // for loop with if statement and break
+        for (int i = 0;  i <= 10; i++) {
+            if (i == 6) {
+                System.out.println("You hit the number 6");
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+        // with continue keyword
+        for (int i = 0;  i <= 10; i++) {
+            if (i == 6) {
+                System.out.println("You hit the number 6");
+                continue;
+            }
+            System.out.println("i = " + i);
         }
     }
 }
