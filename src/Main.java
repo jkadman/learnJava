0d6e620e582e69b7ba3ec90ec5e61597b8560d76
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-05 13:01:45 -0700

    add various operations and examples

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
 public class Main {
     public static void main(String[] args) {
+        float num1 = 5f;
+        int num2 = 6;
+        double num3 = 5;
 
-        System.out.println("Hello Brain!");
+        // operator things which performs operation ie: +, -, * etc
+        // operands things that are use by operators: float, int etc
+        // operations follows order of bedmas
+
+        System.out.println("adding: " + num1 + num2);
+        System.out.println("subtracting: " + (num1 - num2));
+        System.out.println("multiplying" + num1 * num2);
+        System.out.println("dividing with float: " + num1 / num2);
+        System.out.println("dividing with double: " + num3 / num2);
+        System.out.println("modulo: " + num1 % num2);
+
+        int myNumber = 12;
+        myNumber += 4;
+        System.out.println(myNumber);
+
+        myNumber -= 7;
+        System.out.println(myNumber);
+
+        myNumber /= 4;
+        System.out.println(myNumber);
+
+        myNumber *= 4;
+        System.out.println(myNumber);
+
+        int myNumber2 = 3;
+        myNumber2++;
+        System.out.println(myNumber2);
     }
 }
