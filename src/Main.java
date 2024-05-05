5dfc6e3936ce6cef9d1aa264a8d65adca521694e
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-05 14:16:09 -0700

    add basic for loop with increment and decrement examples

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        // basic for loop
+        for (int i = 0;  i <= 10; i++) {
+            System.out.println("i = " + i);
+        }
+
+        for (int j = 10; j >= 0; j--) {
+            System.out.println("j = " + j);
+        }
     }
 }
