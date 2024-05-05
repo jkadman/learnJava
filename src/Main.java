90f01b09ec229506d0522d63c88403f98ab001e6
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-05 13:39:30 -0700

    add example conditional statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-       
+        // some review: = is called assignment operator; == is called equality operators
+        int age = 17;
+        if (age >= 19) {
+            System.out.println("Please come in and enjoy");
+        } else {
+            System.out.println("Your age is only " + age + "." + " Please come back in " + (19 - age) + " years!");
+        }
+
     }
 }
