339fbcedc1f540a1e3dd961fd5ad4754aead938d
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-06 23:14:08 -0700

    basic array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        // array: store multiple values in a variable
+        int[] numbers = {12, 15, 60, 100, 200, 20, 50};
+        // for each statement, different than in JS
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
