6e7b531c14f807374b082267862f767a6eacb73a
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 13:34:59 -0700

    create getters and setters

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,22 @@ public class Main {
         // functionality: the car can drive, change gears
         // objects need a template or plan
 
+        // create new object called instantiation
+            Car car = new Car("Tesla", "S PLAID", "BLACK", 4);
+            Car car2 = new Car("Honda", "FIT", "SILVER", 4);
 
+        System.out.println(car.getName());
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
+        System.out.println(car.getDoors());
+        System.out.println("\n");
+        System.out.println(car2.getName());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getColor());
+        System.out.println(car2.getDoors());
+
+        System.out.println("\n");
+        car.setColor("Red");
+        System.out.println(car.getColor());
     }
 }
