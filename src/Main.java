5f66a38a62e863445b960da31309d9e4d03e0bff
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 12:59:53 -0700

    add notes from his solution

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ public class Main {
     int[] test = {1, 2, 3, 4, 550};
 
     int[] test2 = {66, 70, 4, 100, 3, 55};
-    int number = returnArr(test);
-    int number2 = returnArr(test2);
+    int number = getMax(test);
+    int number2 = getMax(test2);
         System.out.println(number);
         System.out.println(number2);
     }
 
-    public static int returnArr(int[] arr) {
+    public static int getMax(int[] arr) {
+        // only difference between my and his code is that he did:
+        // int high = numbers[0];
         int high = 0;
         for (int num : arr) {
             if (num > high) {
