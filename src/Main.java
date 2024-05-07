6562d7514090800d334b97c90f6ae4daa0dde533
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-06 23:07:40 -0700

    add second function

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         sayHello("Bob", 54);
+
+        int max = getMax(62, 54);
+        System.out.println(max);
     }
 
     public static void sayHello(String name, int age) {
         System.out.println("Hello " + name + "!" + " Your age is " + age);
     }
 
-    
+    public static int getMax(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
 }
 
