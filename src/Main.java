e59a815f627db9cb9c8248523019d8dc03116a89
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 13:05:18 -0700

    add class car

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        // OOP: way of writing code in the way we think about things in the world, take a car
+        // state: what is the make, color of the car
+        // functionality: the car can drive, change gears
+        // objects need a template or plan
 
-        System.out.println("Hello world!");
+
     }
 }
