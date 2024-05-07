fe5093e81a37e6094f20f4a6cbd1184e1c81263f
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-06 23:23:38 -0700

    add formatting

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,20 @@ public class Main {
         for (int number : numbers) {
             System.out.println(number);
         }
-
+        System.out.println("\n");
         // can also use index
         System.out.println("2nd element of numbers: " + numbers[1]);
-
+        System.out.println("\n");
         // classic loop, can use shortcut fori in intellij
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("This is number " + numbers[i]);
         }
-
+        System.out.println("\n");
         // have to add -1 if change i <= numbers.length
         for (int i = 0; i <= numbers.length - 1; i++) {
             System.out.println("This is also number " + numbers[i]);
         }
-
+        System.out.println("\n");
         String[] friends = {"Maria", "Alex", "Bita", "John", "Steve"};
         for (String friend : friends) {
             System.out.println(friend);
