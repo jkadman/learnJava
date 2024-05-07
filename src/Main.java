d05cdccc527bdaaea744b4f69f185d111d03d6a7
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 12:55:54 -0700

    complete challenge

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+    int[] test = {1, 2, 3, 4, 550};
+
+    int[] test2 = {66, 70, 4, 100, 3, 55};
+    int number = returnArr(test);
+    int number2 = returnArr(test2);
+        System.out.println(number);
+        System.out.println(number2);
+    }
+
+    public static int returnArr(int[] arr) {
+        int high = 0;
+        for (int num : arr) {
+            if (num > high) {
+                high = num;
+            }
+        }
+        return high;
     }
 }
